0479c3122616ad8efd69c2a622e0de83ec1a99d1
Ben Mayo <ben@benmayo.com>
2013-02-24 17:00:13 -0500

    title is set

--- spec/requests/backend/backend_dashboards_spec.rb
+++ spec/requests/backend/backend_dashboards_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
 describe "Backend::Dashboards" do
+  let(:base_title) { "BenMayo.com" }
+  
   it "should have the content 'Benmayo.com'" do
     visit '/backend'
-    page.should have_selector('h1', text: "BenMayo.com")
+    page.should have_selector('h1', text: "#{base_title}")
   end
   
   it "should have the right title" do
     visit '/backend'
-    page.should have_selector('title', text: "BenMayo.com :: Dashboard")
+    page.should have_selector('title', text: "#{base_title} :: Dashboard")
   end
 end
