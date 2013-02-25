da4fd428b5151c90b6a6162c26838db72a8e34cf
Ben Mayo <ben@benmayo.com>
2013-02-25 08:21:07 -0500

    Everything is clean and working... about to branch off onto the user model... if things go south come back to this point

--- spec/requests/backend/backend_dashboards_spec.rb
+++ spec/requests/backend/backend_dashboards_spec.rb
@@ -1,15 +1,10 @@
 require 'spec_helper'
 
 describe "Backend::Dashboards" do
-  let(:base_title) { "BenMayo.com" }
-  
-  it "should have the content 'Benmayo.com'" do
-    visit '/backend'
-    page.should have_selector('h1', text: "#{base_title}")
-  end
-  
-  it "should have the right title" do
-    visit '/backend'
-    page.should have_selector('title', text: "#{base_title} :: Dashboard")
-  end
+  before { visit backend_root_path }
+  subject { page }
+
+  it { should have_selector('h1', text: "BenMayo.com") }
+  it { should have_selector('title', text: "BenMayo.com") }
+  it { should have_selector('title', text: ":: Dashboard") }
 end
