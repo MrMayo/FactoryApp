9b83878c00dc5b0c6a483c410a41fb82535bc3e4
Ben Mayo <ben@benmayo.com>
2013-02-25 21:49:14 -0500

    finished testing the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,19 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email, :password_digest
+  attr_accessible :name, :email, :password, :password_confirmation
+  has_secure_password
+
+  before_save { |user| user.email = email.downcase }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name, presence: true,
+                   length: { maximum: 50 }
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true,
+                       length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 end
--- spec/requests/backend/backend_dashboards_spec.rb
+++ spec/requests/backend/backend_dashboards_spec.rb
@@ -1,10 +1,21 @@
 require 'spec_helper'
 
 describe "Backend::Dashboards" do
-  before { visit backend_root_path }
-  subject { page }
+  describe "should have the right titles" do
+    before { visit backend_root_path }
+    subject { page }
 
-  it { should have_selector('h1', text: "BenMayo.com") }
-  it { should have_selector('title', text: "BenMayo.com") }
-  it { should have_selector('title', text: ":: Dashboard") }
+    it { should have_selector('h1', text: "BenMayo.com") }
+    it { should have_selector('title', text: "BenMayo.com") }
+    it { should have_selector('title', text: ":: Dashboard") }
+  end
+
+  describe "should have the right links" do
+    it "and MrMayo should build a test for this"
+  #   visit root_path
+  #   click_link "About"
+  #   page.should have_selector 'title', text: full_title('About Us')
+  #   click_link "Help"
+  #   page.should # fill in
+  end
 end
