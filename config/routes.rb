0346e1619bf9ef44a1561e8ce2cedfd21d401d0a
Ben Mayo <ben@benmayo.com>
2013-02-28 17:02:46 -0500

    sessions controller is functioning and behind the namespacing... all is right with the world

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 FactoryApp::Application.routes.draw do
-  resources :sessions, only: [:new, :create, :destroy]
-  match "/signin",  to: "sessions#new"
-  match "/signout", to: "sessions#destroy", via: :delete
-
   namespace :backend do
     resources :users
+    
+    resources :sessions, only: [:new, :create, :destroy]
+    match "/signin",  to: "sessions#new"
+    match "/signout", to: "sessions#destroy", via: :delete
+    
     root to: 'dashboard#index'
   end
 end
---
+++ spec/helpers/backend/sessions_helper_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+
+describe Backend::SessionsHelper do
+end
